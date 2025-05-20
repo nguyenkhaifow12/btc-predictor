f144fef7bce1e8da5365fc53617a59c228149cd2
nguyenkhaifow12 <nguyenkhaifow@gmail.com>
2025-05-20 19:00:20 +0700

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -4,26 +4,30 @@ import ta
 def calculate_indicators(df):
     df = df.copy()
 
+    close = df['Close']
+    if len(close.shape) > 1:
+        close = close.squeeze()  # ép từ (N,1) thành (N,)
+
     # EMA
-    df['EMA20'] = ta.trend.ema_indicator(df['Close'], window=20)
-    df['EMA50'] = ta.trend.ema_indicator(df['Close'], window=50)
-    df['EMA200'] = ta.trend.ema_indicator(df['Close'], window=200)
+    df['EMA20'] = ta.trend.ema_indicator(close, window=20)
+    df['EMA50'] = ta.trend.ema_indicator(close, window=50)
+    df['EMA200'] = ta.trend.ema_indicator(close, window=200)
 
     # MACD
-    macd = ta.trend.macd(df['Close'])
+    macd = ta.trend.MACD(close)
     df['MACD'] = macd.macd()
     df['MACD_signal'] = macd.macd_signal()
 
     # RSI
-    df['RSI'] = ta.momentum.RSIIndicator(df['Close']).rsi()
+    df['RSI'] = ta.momentum.RSIIndicator(close).rsi()
 
     # Bollinger Bands
-    bb = ta.volatility.BollingerBands(df['Close'])
+    bb = ta.volatility.BollingerBands(close)
     df['BB_upper'] = bb.bollinger_hband()
     df['BB_lower'] = bb.bollinger_lband()
 
     # ADX
-    adx = ta.trend.ADXIndicator(df['High'], df['Low'], df['Close'])
+    adx = ta.trend.ADXIndicator(df['High'], df['Low'], close)
     df['ADX'] = adx.adx()
     df['+DI'] = adx.adx_pos()
     df['-DI'] = adx.adx_neg()
